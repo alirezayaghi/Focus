432b754a7e2d8ab32da80e8fcccf61a333622959
sargardan <soheil22222222@users.noreply.github.com>
2016-05-18 14:47:07 +0430

    Update badword.lua

--- plugins/badword.lua
+++ plugins/badword.lua
@@ -2,7 +2,7 @@
 local function addword(msg, name)
     local hash = 'chat:'..msg.to.id..':badword'
     redis:hset(hash, name, 'newword')
-    return "کلمه جدید به فیلتر کلمات اضافه شد\n>"..name
+    return "کلمه جدید به فیلتر کلمات اضافه شد\n☞"..name
 end
 
 local function get_variables_hash(msg)

2bbf73adf4eb30691641088cb0b2a87a82789740
sargardan <soheil22222222@users.noreply.github.com>
2016-05-18 15:11:30 +0430

    Update inPm.lua

--- plugins/inPm.lua
+++ plugins/inPm.lua
@@ -22,7 +22,7 @@ local service = msg.service
 		else
 			from_username = "@[none]"
 		end
-		text = "User From Info:\n\nID > "..from_id.."\nFirst > "..from_first_name.."\nLast > "..from_last_name.."\nUsername > "..from_username
+		text = "User From Info:\n\nID ☞ "..from_id.."\nFirst ☞ "..from_first_name.."\nLast ☞ "..from_last_name.."\nUsername ☞ "..from_username
 		send_large_msg(user, text)
 	end
 	return msg
@@ -35,7 +35,7 @@ local function chat_list(msg)
     if not data[tostring(groups)] then
         return 'No groups at the moment'
     end
-    local message = '> List of Groups/SuperGroups:\n*Use #join (ID) to join*\n\n'
+    local message = '☞ List of Groups/SuperGroups:\n*Use #join (ID) to join*\n\n'
     for k,v in pairsByKeys(data[tostring(groups)]) do
 		local group_id = v
 		if data[tostring(group_id)] then
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Welcome to BlackPlus!\n\n>To get a new BlackPlus group, contact a support group:\n https://telegram.me/joinchat/A2ETvD160xvoj4y_pNAgig\n\nFor more information, check out our channels:\n\n@Black_CH \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @BlackPlus!"
+		text = "سلام به ربات فوکوس خوش امدین برای دریافت دستورات سوپرگروه دستور /superhelp دریافت کنید  Channel: @Team_Focus"
      	return text
     end
 
@@ -256,3 +256,5 @@ return {
     run = run,
 	pre_process = pre_process
 }
+
+--by @Team-Focus
